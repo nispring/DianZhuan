a4e493f922a3a0e7a40136f91b529c86c5fea612
“liu541100186” <meijust@sina.cn>
2014-06-05 15:53:49 +0800

    Initial Commit

---
+++ DianZhuan/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DianZhuan
+//
+//  Created by 时代合盛 on 14-6-5.
+//  Copyright (c) 2014年 时代合盛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CBAppDelegate class]));
+    }
+}
